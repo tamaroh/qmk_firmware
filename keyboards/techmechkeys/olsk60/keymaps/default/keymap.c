6e31fd07b3101acf679ef3b9e1e80a31d10dea33
techmech <88352328+techmech-keeb@users.noreply.github.com>
2022-12-09 18:46:24 +0900

    keymap edit WIP

--- keyboards/techmechkeys/olsk60/keymaps/default/keymap.c
+++ keyboards/techmechkeys/olsk60/keymaps/default/keymap.c
@@ -3,22 +3,63 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      *    ┌───┬───┬───┬───┬───┬───┐  ┌───┬───┬───┬───┬───┬────────┐
-     *    │ESC│ 1 │ 2 │ 3 │ 4 │ 5 │  │ G │ H │ I │ J │ K │  BKSP  │
+     *    │ESC│ 1 │ 2 │ 3 │ 4 │ 5 │  │ 6 │ 7 │ 8 │ 9 │ 0 │  BKSP  │
      *  ┌─┴───┼───┼───┼───┼───┼───┼  ├───┼───┼───┼───┼───┼───┬────└─┐
-     *  │ Tab │ B │ C │ D │ E │ F │  │ G │ H │ I │ J │ K │ [ │  \   │ 
+     *  │ Tab │ Q │ W │ E │ R │ T │  │ G │ H │ I │ J │ K │ [ │  \   │ 
      * ┌┴─────┼───┼───┼───┼───┼───┼  ├───┼───┼───┼───┼───┼───┴─────┬┘
-     * │ Ctrl │ B │ C │ D │ E │ F │  │ G │ H │ I │ J │ K │ Enter   │     
+     * │ Ctrl │ A │ S │ D │ F │ G │  │ G │ H │ I │ J │ K │ Enter   │     
      * ├──────┼───┼───┼───┼───┼───┼  ├───┼───┼───┼───┼───┼───┤────┬┘
-     * │Shift │ B │ C │ D │ E │ F │  │ G │ H │ I │ J │ K │UP │Sft │   
+     * │Shift │ Z │ X │ C │ V │ B │  │ G │ H │ I │ J │ K │UP │Sft │   
      * └─┬────┼───┼───┼───┼───┼───┼  ├───┼───┼───┼───┼───┼───┼───┬┘
      *   │ A  │ B │ C │ D │ E │ F │  │ G │ H │ I │ J │ K │ L │ L │
      *   └────┴───┴───┴───┴───┴───┘  └───┴───┴───┴───┴───┴───┴───┘
      */
-    [0] = LAYOUT_ortho_5x12(
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L,
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L,
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L,
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L,
-        KC_A,    KC_B,    KC_C,    KC_D,    KC_E,    KC_F,    KC_G,    KC_H,    KC_I,    KC_J,    KC_K,    KC_L
-    )
+    [0] = LAYOUT_ortho60_split_sp(
+        KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC,
+        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_BSLS,
+        KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLNK, KC_ENT,
+        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_UP,    KC_RSFT,
+        KC_LCTL,  KC_LGUI, KC_LALT,          KC_SPC,        MO(1),          KC_DEL,           MO(2),   KC_LEFT,  KC_DOWN,  KC_RGHT
+    ),
+    [1] = LAYOUT_ortho60_625u_sp(
+        KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_BSPC,
+        KC_TAB,   KC_F11,  KC_F12,  KC_UP,   KC_R,    KC_T,        KC_Y,    KC_U,    KC_INS,  KC_O,    KC_PSCR,  KC_LBRC,  KC_INT3,
+        KC_CAPS,  KC_A,    KC_LEFT, KC_DOWN, KC_RGHT, KC_G,        KC_H,    KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,  KC_ENT,
+        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_NUM,  KC_M,    KC_QUOT, KC_DOT,  KC_SLSH,  KC_UP,    KC_RSFT,
+        KC_LCTL,  KC_LGUI, KC_LALT,          KC_SPC,        MO(1),          KC_DEL,           MO(2),   KC_LEFT,  KC_DOWN,  KC_RGHT
+    ),
+    [2] = LAYOUT_ortho60_625u_sp(
+        KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC,
+        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_BSLS,
+        KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLNK, KC_ENT,
+        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_UP,    KC_RSFT,
+        KC_LCTL,  KC_LGUI, KC_LALT,          KC_SPC,        MO(1),          KC_DEL,           MO(2),   KC_LEFT,  KC_DOWN,  KC_RGHT
+    ),
+    [3] = LAYOUT_ortho60_625u_sp(
+        KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC,
+        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_BSLS,
+        KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLNK, KC_ENT,
+        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_UP,    KC_RSFT,
+        KC_LCTL,  KC_LGUI, KC_LALT,          KC_SPC,        MO(1),          KC_DEL,           MO(2),   KC_LEFT,  KC_DOWN,  KC_RGHT
+    ),
+
+    [0] = LAYOUT_ortho60_625u_sp(
+        KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC,
+        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_BSLS,
+        KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLNK, KC_ENT,
+        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_UP,    KC_RSFT,
+        KC_LCTL,  KC_LGUI, KC_LALT,                         MO(1),                            MO(2),   KC_LEFT,  KC_DOWN,  KC_RGHT
+    ),
+    [1] = LAYOUT_ortho60_625u_sp(
+        KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC,
+        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_BSLS,
+        KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLNK, KC_ENT,
+        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_UP,    KC_RSFT,
+        KC_LCTL,  KC_LGUI, KC_LALT,          KC_SPC,        MO(1),          KC_DEL,           MO(2),   KC_LEFT,  KC_DOWN,  KC_RGHT
+    ),
+
+
+
+
+
 };
