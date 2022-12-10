33672ed24b54a53e99d533e859bfa62bbda822d8
Tamaroh <tamaroh@gmail.com>
2022-12-10 23:50:09 +0900

    changed keymap, added reset

--- keyboards/techmechkeys/olsk60/keymaps/tamaroh/keymap.c
+++ keyboards/techmechkeys/olsk60/keymaps/tamaroh/keymap.c
@@ -15,25 +15,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *   └────┴───┴───┴───┴───┴───┘  └───┴───┴───┴───┴───┴───┴───┘
      */
     [0] = LAYOUT_ortho60_split_sp(
-        KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC,
-        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_BSLS,
-        KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_ENT,
-        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_UP,    KC_RSFT,
-        KC_LCTL,  KC_LGUI, KC_LALT,          KC_SPC,        MO(1),          KC_DEL,           MO(2),   KC_LEFT,  KC_DOWN,  KC_RGHT
+        KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,           KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,   KC_BSPC,
+        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_QUOT,  KC_BSLS,
+        KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_ENT,
+        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_UP,    KC_RSFT,
+        KC_LALT,  KC_LGUI, KC_LNG2,          KC_SPC,        LT(1, KC_BSPC),    KC_ENT,           KC_LNG1, KC_LEFT,  KC_DOWN,  KC_RGHT
     ),
     [1] = LAYOUT_ortho60_split_sp(
-        KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_BSPC,
-        KC_TAB,   KC_F11,  KC_F12,  KC_UP,   KC_R,    KC_T,        KC_Y,    KC_U,    KC_INS,  KC_O,    KC_PSCR,  KC_LBRC,  KC_INT3,
-        KC_CAPS,  KC_A,    KC_LEFT, KC_DOWN, KC_RGHT, KC_G,        KC_H,    KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,  KC_ENT,
-        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_NUM,  KC_M,    KC_QUOT, KC_DOT,  KC_SLSH,  KC_UP,    KC_RSFT,
-        KC_LCTL,  KC_LGUI, KC_LALT,          KC_SPC,        MO(1),          KC_DEL,           MO(2),   KC_LEFT,  KC_DOWN,  KC_RGHT
+        KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,       KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,   KC_F12,
+        KC_ESC,   KC_Q,    KC_W,    KC_UP,   KC_R,    KC_T,        KC_Y,       KC_U,       KC_INS,     KC_O,       KC_PSCR,  KC_LBRC,  KC_DEL,
+        KC_CAPS,  KC_A,    KC_LEFT, KC_UP,   KC_RGHT, KC_G,        KC_MINS,    KC_EQL,     KC_LBRC,    KC_RBRC,    KC_NO,    KC_ENT,
+        KC_LSFT,  KC_Z,    KC_HOME, KC_DOWN, KC_END,  KC_B,        S(KC_MINS), S(KC_EQL),  S(KC_LBRC), S(KC_RBRC), KC_SLSH,  KC_UP,    KC_RSFT,
+        KC_LCTL,  KC_LGUI, KC_LNG2,          KC_SPC,        KC_TRNS,           KC_ENT,           MO(2),   KC_LEFT,  KC_DOWN,  KC_RGHT
     ),
     [2] = LAYOUT_ortho60_split_sp(
         KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC,
         KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_BSLS,
         KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_ENT,
         KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_UP,    KC_RSFT,
-        KC_LCTL,  KC_LGUI, KC_LALT,          KC_SPC,        MO(1),          KC_DEL,           MO(2),   KC_LEFT,  KC_DOWN,  KC_RGHT
+        KC_LCTL,  KC_LGUI, QK_BOOT,          KC_SPC,        KC_TRNS,        KC_DEL,           KC_TRNS, KC_LEFT,  KC_DOWN,  KC_RGHT
     ),
     [3] = LAYOUT_ortho60_split_sp(
         KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC,
@@ -43,20 +43,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,  KC_LGUI, KC_LALT,          KC_SPC,        MO(1),          KC_DEL,           MO(2),   KC_LEFT,  KC_DOWN,  KC_RGHT
     ),
 
-    [0] = LAYOUT_ortho60_625u_sp(
-        KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC,
-        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_BSLS,
-        KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_ENT,
-        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_UP,    KC_RSFT,
-        KC_LCTL,  KC_LGUI, KC_LALT,                         MO(1),                            MO(2),   KC_LEFT,  KC_DOWN,  KC_RGHT
-    ),
-    [1] = LAYOUT_ortho60_625u_sp(
-        KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC,
-        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_BSLS,
-        KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_ENT,
-        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_UP,    KC_RSFT,
-        KC_LCTL,  KC_LGUI, KC_LALT,                         MO(1),                            MO(2),   KC_LEFT,  KC_DOWN,  KC_RGHT
-    ),
+    // [0] = LAYOUT_ortho60_625u_sp(
+    //     KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC,
+    //     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_BSLS,
+    //     KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_ENT,
+    //     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_UP,    KC_RSFT,
+    //     KC_LCTL,  KC_LGUI, KC_LALT,                         MO(1),                            MO(2),   KC_LEFT,  KC_DOWN,  KC_RGHT
+    // ),
+    // [1] = LAYOUT_ortho60_625u_sp(
+    //     KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC,
+    //     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_BSLS,
+    //     KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_ENT,
+    //     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_UP,    KC_RSFT,
+    //     KC_LCTL,  KC_LGUI, KC_LALT,                         MO(1),                            MO(2),   KC_LEFT,  KC_DOWN,  KC_RGHT
+    // ),
 
 
 
