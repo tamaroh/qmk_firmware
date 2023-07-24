49ddc70c94af96563a18210dbe6ca23f00f0b78b
Tamaroh <tamaroh@gmail.com>
2023-07-24 23:09:21 +0900

    update 0.21.6, checkout maggie42,44

---
+++ keyboards/olsk60qmk/keymaps/tamaroh/keymap.c
@@ -0,0 +1,34 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
+     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │ K │ L │
+     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │ K │ L │
+     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │ K │ L │
+     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │ K │ L │
+     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+     * │ A │ B │ C │ D │ E │ F │ G │ H │ I │ J │ K │ L │
+     * └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
+     */
+    [0] = LAYOUT_ortho60_split_sp(
+        KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,   KC_BSLS,
+        KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+        KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,    KC_DOT,    KC_SLSH,    KC_UP, KC_RSFT,
+        KC_LALT,    KC_LWIN,    KC_LNG2,    KC_SPC,    LT(1, KC_BSPC),    KC_ENT,    KC_LNG1,    KC_LEFT,    KC_DOWN,    KC_RGHT
+    ),
+    [1] = LAYOUT_ortho60_split_sp(
+        _______,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_DEL,
+        _______,    _______,    KC_HOME,    KC_UP,    KC_END,    KC_PGUP,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_F11,   KC_F12,
+        _______,    _______,    KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_PGDN,    KC_MINS,    KC_EQL,    KC_LBRC,    KC_RBRC,    KC_GRV, _______,
+        _______,    _______,    _______,    _______,    _______,    _______,    KC_UNDS,    KC_PLUS,    KC_LCBR,    KC_RCBR,    KC_TILD,    _______, _______,
+        _______,    _______,    _______,    _______,    LT(1, KC_BSPC),    KC_ENT,    KC_LNG1,    _______,    _______,    _______
+    )
+};
