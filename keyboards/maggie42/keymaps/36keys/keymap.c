4083b0b47954d26c0002d8d874b9a4a24aa93e45
Tamaroh <tamaroh@gmail.com>
2023-08-05 17:59:03 +0900

    add 36keys

--- keyboards/maggie42/keymaps/36keys/keymap.c
+++ keyboards/maggie42/keymaps/36keys/keymap.c
@@ -5,37 +5,37 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /*
-      * ┌───┬───┬───┬───┬───┬───┐   ┌───┬───┬───┬───┬───┬───┐
-      * │Tab│ Q │ W │ E │ R │ T │   │ Y │ U │ I │ O │ P │Bsp│
-      * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
-      * │Ctl│ A │ S │ D │ F │ G │   │ H │ J │ K │ L │ ; │ ' │
-      * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
-      * │Win│ Z │ X │ C │ V │ B │   │ N │ M │ , │ . │ / │Sft│
-      * └───┴───┴───┼───┼───┼───┤   ├───┼───┼───┼───┴───┴───┘
-      *             │Sft│Rse│Spc│   │ BS│Ent│Alt│
-      *             └───┴───┴───┘   └───┴───┴───┘
+      * ┌───┬───┬───┬───┬───┐   ┌───┬───┬───┬───┬───┐
+      * │ Q │ W │ E │ R │ T │   │ Y │ U │ I │ O │ P │
+      * ├───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┤
+      * │ A │ S │ D │ F │ G │   │ H │ J │ K │ L │ ; │
+      * ├───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┤
+      * │ Z │ X │ C │ V │ B │   │ N │ M │ , │ . │ / │
+      * └───┴───┼───┼───┼───┤   ├───┼───┼───┼───┴───┘
+      *         │Win│Tab│Spc│   │ BS│Ent│Alt│
+      *         └───┴───┴───┘   └───┴───┴───┘
       */
     [0] = LAYOUT(
-        KC_NO,  KC_Q,           KC_W,    KC_E,    KC_R,    KC_T,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,            KC_NO,
-        KC_NO, LCTL_T(KC_A),    KC_S,    KC_D,    KC_F,    KC_G,      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,         KC_NO,
-        KC_NO, LGUI_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,      KC_N,    KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH), KC_NO,
-                                         KC_LSFT, MO(1), KC_SPC,      KC_BSPC, LT(2, KC_ENT),KC_RALT
+        KC_NO, KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,            KC_NO,
+        KC_NO, KC_A,     KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    RCTL_T(KC_SCLN), KC_NO,
+        KC_NO, KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH), KC_NO,
+                             KC_LGUI, LT(1, KC_TAB), KC_SPC,      KC_BSPC, LT(2, KC_ENT),KC_RALT
     ),
     [1] = LAYOUT(
         _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-        _______, KC_LNG2, KC_LEFT, KC_UP,   KC_RGHT, KC_PGUP,         KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, _______,
-        _______, KC_LNG1, KC_HOME, KC_DOWN, KC_END,  KC_PGDN,         KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, _______,
+        _______, KC_LNG2, KC_LEFT, KC_UP,   KC_RGHT, KC_PGUP,         _______, _______, _______, _______, _______, _______,
+        _______, KC_LNG1, KC_HOME, KC_DOWN, KC_END,  KC_PGDN,         _______, _______, _______, _______, _______, _______,
                                    _______, _______, _______,         KC_DEL,  MO(3), _______
     ),
     [2] = LAYOUT(
         _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
-        _______, KC_LNG2, KC_LEFT, KC_UP,   KC_RGHT, KC_PGUP,         KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, _______,
-        _______, _______, _______, _______, _______, _______,         KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, _______,
+        _______, KC_ESC,  _______, KC_GRV,  KC_QUOT, _______,         KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, _______,
+        _______, _______, _______, KC_TILD, KC_DQUO, _______,         KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, _______,
                                    _______,   MO(3), _______,         _______, _______, _______
     ),
     [3] = LAYOUT(
-        _______,   KC_F1,  KC_F2 ,   KC_F3,   KC_F4,   KC_F5,         KC_F6,     KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
-        _______, _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______,  KC_F12,
+        _______,   KC_F1,  KC_F2 ,   KC_F3,   KC_F4,   KC_F5,         KC_F6,     KC_F7,   KC_F8,   KC_F9,  KC_F10, _______,
+        _______, _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______, _______,
                                    _______, _______, _______,         _______, _______, _______
     )
