cb2c4817d49fbbe97111b5f762057654bc22ac0a
Tamaroh <tamaroh@gmail.com>
2023-11-19 21:22:42 +0900

    compile worked without VIA

---
+++ keyboards/tamaroh/maggie54t/keymaps/default/keymap.c
@@ -0,0 +1,46 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┐
+     * │ 7 │ 8 │ 9 │ / │
+     * ├───┼───┼───┼───┤
+     * │ 4 │ 5 │ 6 │ * │
+     * ├───┼───┼───┼───┤
+     * │ 1 │ 2 │ 3 │ - │
+     * ├───┼───┼───┼───┤
+     * │ 0 │ . │Ent│ + │
+     * └───┴───┴───┴───┘
+     */
+    [0] = LAYOUT(
+        KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,
+        KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,
+        KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,
+        KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,
+        KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1
+    ),
+    [1] = LAYOUT(
+        KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,
+        KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,
+        KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,
+        KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,
+        KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1
+    ),
+    [2] = LAYOUT(
+        KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,
+        KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,
+        KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,
+        KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,
+        KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1
+    ),
+    [3] = LAYOUT(
+        KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,
+        KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,
+        KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,
+        KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,
+        KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1,KC_1
+    ),
+};
