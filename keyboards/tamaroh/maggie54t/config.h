b98e263cc51abc542e2324d15aa5592fca9162b9
Tamaroh <tamaroh@gmail.com>
2023-11-26 00:48:30 +0900

    rgb keycode now work

--- keyboards/tamaroh/maggie54t/config.h
+++ keyboards/tamaroh/maggie54t/config.h
@@ -8,20 +8,25 @@
  */
 
 /* disable debug print */
-//#define NO_DEBUG
+// #define NO_DEBUG
 
 /* disable print */
-//#define NO_PRINT
+// #define NO_PRINT
 
 /* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
+// #define NO_ACTION_LAYER
+// #define NO_ACTION_TAPPING
+// #define NO_ACTION_ONESHOT
+// for reducing memory usage
 #define DYNAMIC_KEYMAP_LAYER_COUNT 3
 
+// LED setting
+#define RGBLIGHT_DEFAULT_HUE 180
+
+// pointing device setting
 #define ADNS5050_SCLK_PIN D0
 #define ADNS5050_SDIO_PIN D1
 #define ADNS5050_CS_PIN   D4
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-// only required if not setting mouse layer elsewhere
 #define AUTO_MOUSE_DEFAULT_LAYER 1
+
