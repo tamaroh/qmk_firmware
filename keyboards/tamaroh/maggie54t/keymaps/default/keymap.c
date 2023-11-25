0027de47e33a5872436233f79c02e4e32ceece79
Tamaroh <tamaroh@gmail.com>
2023-11-25 17:10:55 +0900

    trackball disabled, fixed keymap

--- keyboards/tamaroh/maggie54t/keymaps/default/keymap.c
+++ keyboards/tamaroh/maggie54t/keymaps/default/keymap.c
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
+#include "print.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
@@ -38,6 +39,21 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     return false;
 }
 
-void pointing_device_init_user(void) {
-    set_auto_mouse_enable(true);         // always required before the auto mouse feature will work
+    // void pointing_device_init_user(void) {
+    //     set_auto_mouse_enable(true);         // always required before the auto mouse feature will work
+    // }
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+//   debug_matrix=true;
+  //debug_keyboard=true;
+  //debug_mouse=true;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  // If console is enabled, it will print the matrix position and status of each key pressed
+#ifdef CONSOLE_ENABLE
+    uprintf("KL: kc: 0x%04X, row: %2u, col: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.row, record->event.key.col, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+#endif 
+  return true;
 }
