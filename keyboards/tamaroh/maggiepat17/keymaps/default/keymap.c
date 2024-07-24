a73fddb5a8584a01ecb53d3fe998bc512f0adafe
Tamaroh <tamaroh@gmail.com>
2024-07-24 23:16:42 +0900

    create info.json, default keymap

--- keyboards/tamaroh/maggiepat17/keymaps/default/keymap.c
+++ keyboards/tamaroh/maggiepat17/keymaps/default/keymap.c
@@ -4,45 +4,52 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * ┌───┬───┬───┬───┐
-     * │TG1│ / │ * │ - │
-     * ├───┼───┼───┼───┤
-     * │ 7 │ 8 │ 9 │   │
-     * ├───┼───┼───┤ + │
-     * │ 4 │ 5 │ 6 │   │
-     * ├───┼───┼───┼───┤
-     * │ 1 │ 2 │ 3 │   │
-     * ├───┴───┼───┤Ent│
-     * │   0   │ . │   │
-     * └───────┴───┴───┘
-     */
-    [0] = LAYOUT_numpad_5x4(
-        TG(1),   KC_PSLS, KC_PAST, KC_PMNS,
-        KC_P7,   KC_P8,   KC_P9,
-        KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
-        KC_P1,   KC_P2,   KC_P3,
-        KC_P0,   KC_PDOT,          KC_PENT
-    ),
+	[0] = LAYOUT(
+		KC_ESC,  KC_Q, KC_W, KC_E, KC_R,
+        KC_LCTL, KC_A, KC_S, KC_D, KC_F,
+		KC_LSFT, KC_Z, KC_X, KC_C, KC_V,
+		KC_SPC,  KC_SPC
+	),
 
-    /*
-     * ┌───┬───┬───┬───┐
-     * │TG1│ / │ * │ - │
-     * ┌───┬───┬───┐───┤
-     * │Hom│ ↑ │PgU│   │
-     * ├───┼───┼───┤ + │
-     * │ ← │   │ → │   │
-     * ├───┼───┼───┤───┤
-     * │End│ ↓ │PgD│   │
-     * ├───┴───┼───┤Ent│
-     * │Insert │Del│   │
-     * └───────┴───┘───┘
-     */
-    [1] = LAYOUT_numpad_5x4(
-        _______, _______, _______, _______,
-        KC_HOME, KC_UP,   KC_PGUP,
-        KC_LEFT, XXXXXXX, KC_RGHT, _______,
-        KC_END,  KC_DOWN, KC_PGDN,
-        KC_INS,           KC_DEL,  _______
-    )
+	[1] = LAYOUT(
+		KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS,
+		KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS,
+		KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS,
+		KC_TRNS,
+		KC_TRNS,
+		KC_TRNS
+	),
+
+	[2] = LAYOUT(
+		KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS,
+		KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS,
+		KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS,
+		KC_TRNS,
+		KC_TRNS,
+		KC_TRNS
+	),
+
+	[3] = LAYOUT(
+		KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS,
+		KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS,
+		KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS,
+		KC_TRNS,
+		KC_TRNS,
+		KC_TRNS
+	),
 };
