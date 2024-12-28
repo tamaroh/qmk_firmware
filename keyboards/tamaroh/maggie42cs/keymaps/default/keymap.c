a78f557e567f0c4c7fd941b80c965fd16f3878e9
Tamaroh <tamaroh@gmail.com>
2024-12-29 00:23:01 +0900

    42cs, 42cs0: rename

--- keyboards/tamaroh/maggie42cs/keymaps/default/keymap.c
+++ keyboards/tamaroh/maggie42cs/keymaps/default/keymap.c
@@ -19,13 +19,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                       └───┘   └───┘
       */
     [0] = LAYOUT(
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                              KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                   KC_LGUI, MO(1), KC_SPC,             KC_BSPC,  LT(2, KC_ENT),  KC_RALT
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                                   KC_LGUI, MO(1), KC_SPC, KC_BSPC,  LT(2, KC_ENT),  KC_RALT
     ),
     [1] = LAYOUT(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_ESC,
+        KC_ESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PIPE,
         _______, KC_LNG2, KC_LEFT, KC_UP,   KC_RGHT, KC_PGUP,                            KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_COLN, KC_DQUO,
         _______, KC_LNG1, KC_HOME, KC_DOWN, KC_END,  KC_PGDN,                            KC_UNDS, KC_PLUS, KC_LABK, KC_RABK, KC_QUES, KC_TILD,
                                    _______, _______, _______,            KC_DEL,  MO(3), _______
@@ -38,8 +38,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [3] = LAYOUT(
         _______,   KC_F1,  KC_F2 ,   KC_F3,   KC_F4,   KC_F5,                            KC_F6,     KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
-        _______, _______, LCTL(KC_LEFT), _______, LCTL(KC_RGHT), _______,                _______, LGUI(KC_1), _______, _______, _______,  KC_F12,
-        _______, _______, _______, _______, _______, _______,                            _______, LCTL(KC_GRV), _______, _______, _______, _______,
+        _______, _______, LCTL(KC_LEFT), LCTL(KC_UP), LCTL(KC_RGHT), _______,                _______, LGUI(KC_1), _______, _______, _______,  KC_F12,
+        _______, _______, _______, LCTL(KC_DOWN), _______, _______,                            _______, LCTL(KC_GRV), _______, _______, _______, _______,
                                    _______, _______, _______,          _______, _______, _______
     )
 };
